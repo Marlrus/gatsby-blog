c3802ec93ea45a99295f0dc648c4b15280799882
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-07-20 14:46:34 -0500

    Added robots.txt plugin for SEOs and lighthouse error correction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,57 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import styled from 'styled-components';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const BlogLink = styled(Link)`
+	text-decoration: none;
+`;
 
 const IndexPage = ({ data }) => {
-  console.log(data)
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <div>
-        <h1>Julian's Thawgts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
-            <p>{node.excerpt}</p>
-          </div>
-        ))}
-      </div>
-    </Layout>
-  )
-}
+	console.log(data);
+	return (
+		<Layout>
+			<SEO title='Home' />
+			<div>
+				<h1>Julian's Thawgts</h1>
+				<h4>{data.allMarkdownRemark.totalCount}</h4>
+				{data.allMarkdownRemark.edges.map(({ node }) => (
+					<div key={node.id}>
+						<BlogLink to={node.fields.slug}>
+							<span>
+								{node.frontmatter.title} - {node.frontmatter.date}
+							</span>
+						</BlogLink>
+						<p>{node.excerpt}</p>
+					</div>
+				))}
+			</div>
+		</Layout>
+	);
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            date
-            description
-            title
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
+	query {
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						date
+						description
+						title
+					}
+					fields {
+						slug
+					}
+					excerpt
+				}
+			}
+		}
+	}
+`;
