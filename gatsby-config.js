34c5af43e02cfde1e8dfc3b30a80d368c7382a7b
Marlrus <36910211+Marlrus@users.noreply.github.com>
2020-07-20 16:18:50 -0500

    PWA Capabilities test

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,8 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `Julian's Blog`,
+				short_name: `Julian's Blog`,
 				start_url: `/`,
 				background_color: `#663399`,
 				theme_color: `#663399`,
@@ -45,6 +45,6 @@ module.exports = {
 		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
+		`gatsby-plugin-offline`,
 	],
 };
